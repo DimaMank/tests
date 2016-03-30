663b81492cf026e46d8eb5ded448c406e71e8a93
DimaMank <dima.mank@gmail.com>
2016-03-30 19:53:15 +0300

    First commit

---
+++ Script.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Dima on 30.03.2016.
+ */
+///**
+// * Created by Dima on 16.03.2016.
+//*/
+// (function () {
+//
+//    var perms = ['+','-','/', '*' ],
+//        calc = document.getElementById('calc'),
+//        calc_res = document.getElementById('calculation'),
+//        res = document.getElementById('res');
+//
+//
+//    if (typeof calc.value == 'number'){
+//    calc_res.onclick = function () {
+//
+//        {
+//            res.innerHTML = eval(calc.value);
+//        }
+//    }
+//    }
+//        else {
+//            res.innerHTML = 'ERROR';
+//        }
+// Решение через Eval
+
+
+var calc = '8+43x6';
+var arrMath = ['1','2','3','4','5','6','7','8','9','0','+','-',':','x','(',')']
+function isMathExpretion(){
+    var index;
+    for (index = 0; index < calc.length; index++) {
+        console.log(calc[index]);
+        var quest = calc[index];
+        if (quest in arrMath) {console.log("True, это мат выражение!");}
+        else {alert('false, это не мат выражение!');
+            break}
+
+    }
+}
+isMathExpretion();
+
+function calculat(){
+    var index;
+    for (index = 0; index < calc.length; index++) {
+        console.log(calc[index]);
+        var elem = calc[index];
+        if (elem === "x") {'index--' * 'index++'}
+        if (elem === "+") {"previousElementSibling" * "nextElementSibling"}
+        else {alert('ПОдсчитали!');
+            break}
+
+    }
+}
