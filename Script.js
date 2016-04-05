bffa461b63706af43a1af2e42ce49c3e14daf46b
olegg <olegg.net@gmail.com>
2016-04-05 18:47:20 +0300

    Improve codebase view only

--- Script.js
+++ Script.js
@@ -27,50 +27,99 @@
 
 
 var calc = '8+43x6';
-var arrMath = ['1','2','3','4','5','6','7','8','9','0','+','-',':','x','(',')']
-function isMathExpretion(){
+var arrMath = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', ':', 'x', '(', ')'];
+
+isMathExpression();
+
+// PRIVATE
+
+function isMathExpression() {
     var index;
+    var quest;
+
     for (index = 0; index < calc.length; index++) {
         console.log(calc[index]);
-        var quest = calc[index];
-        if (quest in arrMath) {console.log("True, это мат выражение!");}
-        else {alert('false, это не мат выражение!');
-            }
+        quest = calc[index];
+
+        if (quest in arrMath) {
+            console.log('True, это мат выражение!');
+
+        } else {
+            alert('false, это не мат выражение!');
+        }
 
     }
 }
-isMathExpretion();
 
+function calculat() {
+    var parent, index;
 
-    function calculat(){
+    for (index = 0; index < calc.length; index++) {
+        var elem = calc[index];
+        var mult, sum, res, deg;
+
+        console.log(calc[index]);
+
+        if (elem === 'x') {
+            mult = calc[index - 1] * calc[index + 1];
+
+        } else if (elem === '+') {
+            sum = calc[index - 1] + calc[index + 1];
+
+        } else if (elem === '-') {
+            res = calc[index - 1] - calc[index + 1];
+
+        } else if (elem === ':') {
+            deg = calc[index - 1] / calc[index + 1];
+
+        } else if (elem === '(') {
+            parenthesis();
+            
+        } else {
+            console.log('Ready');
+        }
+
+        if ((index + 1) > calc.length) {
+            break
+
+        } else if (index < 0) {
+            break
+
+        } else {
+            console.log('Продолжаем')
+        }
+
+    }
+
+    parent = parenthesis();
+
+    function parenthesis() {
+        var index, elem, mult, sum, res, deg;
 
         for (index = 0; index < calc.length; index++) {
             console.log(calc[index]);
-            var elem = calc[index];
-            if (elem === "x") {var mult = calc[index-1] * calc[index + 1]}
-            else if (elem === "+") {var sum = calc[index-1] + calc[index + 1]}
-            else if (elem === "-") {var res = calc[index-1] - calc[index + 1]}
-            else if (elem === ":") {var deg = calc[index-1] / calc[index + 1]}
-            else if (elem === "(") {parenthesis()}
-            else {console.log("Ready")}
-            if ((index + 1) > calc.length ) {break}
-            else if (index < 0) {break}
-            else {console.log("Продолжаем")
+            elem = calc[index];
+
+            if (elem === 'x') {
+                mult = calc[index - 1] * calc[index + 1]
+
+            } else if (elem === '+') {
+                sum = calc[index - 1] + calc[index + 1]
+
+            } else if (elem === '-') {
+                res = calc[index - 1] - calc[index + 1]
+
+            } else if (elem === ':') {
+                deg = calc[index - 1] / calc[index + 1]
+
+            } else if (elem === ')') {
+                break
+
+            }
+            else {
+                console.log('Error')
             }
 
         }
-        var parent = parenthesis();
-        function parenthesis(){
-            for (index = 0; index < calc.length; index++) {
-                console.log(calc[index]);
-                var elem = calc[index];
-                if (elem === "x") {var mult = calc[index-1] * calc[index + 1]}
-                else if (elem === "+") {var sum = calc[index-1] + calc[index + 1]}
-                else if (elem === "-") {var res = calc[index-1] - calc[index + 1]}
-                else if (elem === ":") {var deg = calc[index-1] / calc[index + 1]}
-                else if (elem === ")") {break}
-                else {console.log("Error")}
-
-            }}
-        var parent = parenthesis();
     }
+}
