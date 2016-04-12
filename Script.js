346f82b184b5e1fb88a969646eb43c0c08404da4
DimaMank <dima.mank@gmail.com>
2016-04-12 12:26:45 +0300

    Добавлены кнопки проверки и подсчета

--- Script.js
+++ Script.js
@@ -1,65 +1,35 @@
-/**
- * Created by Dima on 30.03.2016.
- */
-///**
-// * Created by Dima on 16.03.2016.
-//*/
-// (function () {
-//
-//    var perms = ['+','-','/', '*' ],
-//        calc = document.getElementById('calc'),
-//        calc_res = document.getElementById('calculation'),
-//        res = document.getElementById('res');
-//
-//
-//    if (typeof calc.value == 'number'){
-//    calc_res.onclick = function () {
-//
-//        {
-//            res.innerHTML = eval(calc.value);
-//        }
-//    }
-//    }
-//        else {
-//            res.innerHTML = 'ERROR';
-//        }
-// Решение через Eval
-
-
-var calc = '8+43x6';
+var calc = '2x2+2';
 var arrMath = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', ':', 'x', '(', ')'];
-
-isMathExpression();
 
 // PRIVATE
 
 function isMathExpression() {
     var index;
     var quest;
-    var resultat;
 
     for (index = 0; index < calc.length; index++) {
         console.log(calc[index]);
         quest = calc[index];
 
         if (arrMath.indexOf(quest) >= 0) {
-            resultat = 'True, это мат выражение!';
+            result = true;
 
         } else {
-            resultat = 'False, это не мат выражение!';
+            result = false;
             break
         }
     }
-    alert(resultat);
+
+    checked.innerHTML = result;
 }
 //Проверка
 
 function calculat() {
-    var parent, index;
+    var index, mult, sum, res, deg, brackets;
 
     for (index = 0; index < calc.length; index++) {
         var elem = calc[index];
-        var mult, sum, res, deg;
+
 
         console.log(calc[index]);
 
@@ -76,26 +46,17 @@ function calculat() {
             deg = calc[index - 1] / calc[index + 1];
 
         } else if (elem === '(') {
-            parenthesis();
+            brackets = parenthesis();
 
         } else {
-            console.log('Ready');
-        }
-
-        if ((index + 1) > calc.length) {
-            break
-
-        } else if (index < 0) {
-            break
-
-        } else {
-            console.log('Продолжаем')
+            console.log(mult);
         }
 
     }
 
-    parent = parenthesis();
+    result = mult + sum + res + deg + brackets;
 
+    outcome.innerHTML = result;
     function parenthesis() {
         var index, elem, mult, sum, res, deg;
 
